fa29f2d44570f753c3255f0d5ddbfdc1042c42f5
tatoGtato <73746637+tatoGtato@users.noreply.github.com>
2023-05-16 22:22:56 -0500

    Add files via upload

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/santiago/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/santiago/Documents/Universidad/2023-1/Desarrollo/proyecto/sprint2" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/santiago/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/santiago/Documents/Universidad/2023-1/Desarrollo/proyecto/sprint2"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/santiago/Documents/Universidad/2023-1/Desarrollo/proyecto/sprint2/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/santiago/Documents/Universidad/2023-1/Desarrollo/proyecto/sprint2/lib/main.dart"
+)
